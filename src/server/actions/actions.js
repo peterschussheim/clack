4c222c3d443901135cb1da476b3c989fa31f1125
Peter Schussheim <peter@schussheim.com>
2017-02-18 14:11:21 -0500

    refactor(server/actions): fix incorrect action names

--- src/server/actions/actions.js
+++ src/server/actions/actions.js
@@ -1,9 +1,11 @@
 export const ADD_MESSAGE_SUCCESS = 'clack/messages/add/success';
-export const RECEIVE_POST = 'clack/messages/receive/add';
+export const RECEIVE_MESSAGE = 'clack/messages/receive/add';
+export const RECEIVE_ROOM = 'clack/messages/receive-all';
+
 export const RENAME_SESSION = 'clack/session/rename';
-export const RECEIVE_ROOM = 'clack/session/messages/receive-all';
 export const RECEIVE_SESSION_NAME = 'clack/session/receive/rename';
 export const JOIN_SESSION = 'clack/session/join';
 export const LEAVE_SESSION = 'clack/session/leave';
 export const RECEIVE_CLIENT_LIST = 'clack/session/receive/client-list';
+
 export const LOGIN_SUCCESS = 'clack/user/login/success';
